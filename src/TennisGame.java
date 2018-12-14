1e9c755a22a6471867f5b455b8937da34894224d
HUNGDT <hk2t.thaihung@gmail.com>
2018-12-14 14:06:24 +0700

    done

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,61 +1,73 @@
 public class TennisGame {
 
-    public static String getScore(String player1Name, String player2Name, int m_score1, int m_score2) {
+    public static String getScore(String firstPlayerName, String secondPlayerName, int scoreOfFirstPlayer, int scoreOfSecondPlayer) {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                    score = "Love-All";
-                    break;
-                case 1:
-                    score = "Fifteen-All";
-                    break;
-                case 2:
-                    score = "Thirty-All";
-                    break;
-                case 3:
-                    score = "Forty-All";
-                    break;
-                default:
-                    score = "Deuce";
-                    break;
+        int defaultScore = 0;
+        if (scoreOfFirstPlayer == scoreOfSecondPlayer) {
+            score = getString(scoreOfFirstPlayer);
 
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
-                    case 0:
-                        score+="Love";
-                        break;
-                    case 1:
-                        score+="Fifteen";
-                        break;
-                    case 2:
-                        score+="Thirty";
-                        break;
-                    case 3:
-                        score+="Forty";
-                        break;
+        } else if (scoreOfFirstPlayer >= 4 || scoreOfSecondPlayer >= 4) {
+            score = getString(scoreOfFirstPlayer, scoreOfSecondPlayer);
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) defaultScore = scoreOfFirstPlayer;
+                else {
+                    score += "-";
+                    defaultScore = scoreOfSecondPlayer;
                 }
+                score = getString(score, defaultScore);
             }
         }
+        return score;
+    }
+
+    private static String getString(int scoreOfFirstPlayer) {
+        String score;
+        switch (scoreOfFirstPlayer) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            case 3:
+                score = "Forty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+        }
+        return score;
+    }
+
+    private static String getString(String score, int defaultScore) {
+        switch (defaultScore) {
+            case 0:
+                score += "Love";
+                break;
+            case 1:
+                score += "Fifteen";
+                break;
+            case 2:
+                score += "Thirty";
+                break;
+            case 3:
+                score += "Forty";
+                break;
+        }
+        return score;
+    }
+
+    private static String getString(int scoreOfFirstPlayer, int scoreOfSecondPlayer) {
+        String score;
+        int differenceScore = scoreOfFirstPlayer - scoreOfSecondPlayer;
+        if (differenceScore == 1) score = "Advantage player1";
+        else if (differenceScore == -1) score = "Advantage player2";
+        else if (differenceScore >= 2) score = "Win for player1";
+        else score = "Win for player2";
         return score;
     }
 }
